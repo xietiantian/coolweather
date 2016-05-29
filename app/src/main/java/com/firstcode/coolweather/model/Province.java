258c045707beedbc131d3e82f0173894837307e5
xietianrui <xtr@bupt.edu.cn>
2016-05-29 19:30:20 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/firstcode/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.firstcode.coolweather.model;
+
+/**
+ * Created by tiantian on 16-5-29.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
